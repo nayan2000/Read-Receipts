44c65ea1e3d2aa59bef7e2e482f3d11aaac3e8aa
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-10-29 21:33:23 +0530

    Pusher Client set up, Chat View

--- message/views.py
+++ message/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from message.models import Conversation
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.decoratos.csrf import csrf_exempt
+from pusher_message.settings_config.keyconfig import *
+from pusher import Pusher
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+#Initialising Pusher Creds
+pusher_client = Pusher(
+    app_id = APP_ID,
+    key = KEY,
+    secret = SECRET,
+    cluster = CLUSTER,
+    ssl = True
+)
+
+@login_required(login_url='login/')
+def index(request):
+    return render(request, "chat.html")
