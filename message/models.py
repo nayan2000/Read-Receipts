cb08c7c7fdcc3fd4b350c544f070c9e8b06c3601
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-10-29 21:21:56 +0530

    Models, Admin Site, Meta data, Migrations complete

--- message/models.py
+++ message/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Conversation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(blank=True, null=True, max_length=400)
+    status = models.CharField(blank=True, null=True, max_length=400)
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name= 'Conversation'
+        verbose_name_plural='Conversations'
